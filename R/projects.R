11f540619e694c85076f27255d12d2fcbfe41a5e
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2017-03-11 18:10:51 +0100

    first version of functions reading all projects and individual project based on hid

---
+++ R/projects.R
@@ -0,0 +1,67 @@
+get_projects <- function() {
+  #' Gets list of available projects
+  api_version <- "v1"
+  api_url_projects <- paste("https://mljar.com/api/", api_version, "/projects" , sep="")
+  rp <- get_json_from_query(api_url_projects)
+  resp <- rp$resp
+  parsed <- rp$parsed
+  
+  structure(
+    list(
+      projects = parsed,
+      response = resp
+    ),
+    class = "get_projects"
+  )
+}
+
+print.get_projects <- function(x, ...) {
+  cat("<MLJAR projects >\n", sep = "")
+  str(x$projects)
+  invisible(x)
+}
+
+get_project <- function(hid) {
+  #' Get data from a project of specified hid
+  api_version <- "v1"
+  api_url_project_hid <- paste("https://mljar.com/api/", api_version, "/projects/", hid, sep="")
+  rp <- get_json_from_query(api_url_project_hid)
+  resp <- rp$resp
+  parsed <- rp$parsed
+
+  structure(
+    list(
+      projects = parsed,
+      response = resp
+    ),
+    class = "get_project"
+  )
+}
+
+get_json_from_query <- function(query){
+  # returns api response and parsed output
+  token <- get_token()
+  resp <- GET(query, add_headers(Authorization = paste("Token", token)))
+  parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
+  
+  if (status_code(resp) != 200) {
+    stop(
+      sprintf(
+        "MLJAR API request failed [%s]\n%s\n<%s>", 
+        status_code(resp),
+        parsed$message,
+        parsed$documentation_url
+      ),
+      call. = FALSE
+    )
+  }
+  return(list(resp=resp, parsed=parsed))
+}
+
+get_token <- function(){
+  token <- Sys.getenv("MLJAR_TOKEN")
+  if (nchar(token)==0) {
+    stop("Specify MLJAR_TOKEN env variable", call. = FALSE)
+  }
+  return(token)
+}
