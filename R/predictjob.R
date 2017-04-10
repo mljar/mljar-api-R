31f9bb2afb41276b1bb5c0e16ab1f93a181a2403
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2017-04-10 22:47:01 +0200

    JSON structure wrapper added to list in predictjob

--- R/predictjob.R
+++ R/predictjob.R
@@ -1,10 +1,11 @@
 submit_predict_job <- function(project_hid, dataset_hid, result_hid){
   
-  data <- list( predict_params = list(project_id =  project_hid,
-                                project_hardware = 'cloud',
-                                algorithms_ids = list(result_hid),
-                                dataset_id = dataset_hid,
-                                cv_models = 1)
+  data <- list(predict_params = toJSON(list(project_id =  project_hid,
+                                            project_hardware = 'cloud',
+                                            algorithms_ids = list(result_hid),
+                                            dataset_id = dataset_hid,
+                                            cv_models = 1),
+                                       auto_unbox =TRUE)
               )
   query <- paste("https://mljar.com/api/", API_VERSION, "/predict/" , sep="")
 
