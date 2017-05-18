26816ac8baba8c05e56ada21ee98a2a294eeafab
Dominik Krzeminski <raymon92@gmail.com>
2017-05-18 13:46:42 +0200

    results functinality added

--- R/result.R
+++ R/result.R
@@ -1,11 +1,12 @@
 get_results <- function(project_hid, experiment_hid){
   #' Get results - list all models.
+  token <- .get_token()
   api_url_results <- paste("https://mljar.com/api/", API_VERSION, "/results/" , sep="")
-  data <- list( project_id =  project_hid,
-                experiment_id =  experiment_hid)
+  datares <- list( project_id =  project_hid,
+                   experiment_id =  experiment_hid)
   resp <- POST(api_url_results, add_headers(Authorization = paste("Token", token)),
-               body = data, encode = "form")
-  .check_response_status(resp, 201)
+               body = datares, encode = "form")
+  .check_response_status(resp, 200)
   parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
   structure(
     list(
@@ -14,4 +15,10 @@ get_results <- function(project_hid, experiment_hid){
     ),
     class = "get_results"
   )
+}
+
+print.get_results <- function(x, ...) {
+  cat("<MLJAR results >\n", sep = "")
+  str(x$results)
+  invisible(x)
 }
---
+++ tests/testthat/test_results.R
@@ -0,0 +1,40 @@
+library(mljar)
+context("Test experiment")
+
+task <- "bin_class"
+create_project('ds', task, 'some description')
+gp <- get_projects()
+for(i in 1:length(gp$projects)) {
+  if (gp$projects[[i]]$title=="ds"){
+    break
+  }
+}
+hid <- gp$projects[[i]]$hid
+
+pr_task <- gp$projects[[i]]$task
+file_from_resources <- "resources/binary_part_iris_converted.csv"
+dataset1 <- add_dataset_if_not_exists(hid, file_from_resources, "test-exp1")
+
+validation_kfolds <- 5
+validation_shuffle <- TRUE
+validation_stratify <-TRUE
+validation_train_split <- NULL
+validation <- "5-fold CV, Shuffle, Stratify"
+algorithms <- c("xgb")
+metric <- "logloss"
+tuning_mode <- "Normal"
+time_constraint <- 1
+create_ensemble <- FALSE
+dataset_preproc <- {}
+
+exp <- add_experiment_if_not_exists(hid, dataset1$dataset, NULL, "exp",
+                                    pr_task, validation_kfolds, validation_shuffle,
+                                    validation_stratify, validation_train_split, algorithms, metric,
+                                    tuning_mode, time_constraint, create_ensemble)
+
+test_that("test get_results", {
+  r<-get_results(hid,exp$hid)
+  expect_equal(length(r$results), 14)
+})
+
+delete_project(hid)
