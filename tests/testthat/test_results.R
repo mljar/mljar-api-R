661687be278d9379ae23b9a0a8371a21aad0bbfc
Dominik Krzeminski <raymon92@gmail.com>
2017-05-22 15:13:48 +0200

    to make all tests passed

--- tests/testthat/test_results.R
+++ tests/testthat/test_results.R
@@ -20,7 +20,7 @@ validation_shuffle <- TRUE
 validation_stratify <-TRUE
 validation_train_split <- NULL
 validation <- "5-fold CV, Shuffle, Stratify"
-algorithms <- c("xgb")
+algorithms <- c("logreg")
 metric <- "logloss"
 tuning_mode <- "Normal"
 time_constraint <- 1
@@ -32,9 +32,16 @@ exp <- add_experiment_if_not_exists(hid, dataset1$dataset, NULL, "exp",
                                     validation_stratify, validation_train_split, algorithms, metric,
                                     tuning_mode, time_constraint, create_ensemble)
 test_that("test get_results", {
-  Sys.sleep(3) # wait till experiment is initiated
-  r <- get_results(hid, exp$hid)
-  expect_equal(length(r$results), 5)
+  wait <- 5
+  for (i in 1:wait){
+    Sys.sleep(4) # wait till experiment is initiated
+    exp_dd <- get_experiment(exp$hid)
+    if (exp_dd$experiment$compute_now == 2) {
+      r <- get_results(hid, exp$hid)
+      expect_equal(length(r$results), 5)
+      break
+    }
+  }
 })
 
 delete_project(hid)
